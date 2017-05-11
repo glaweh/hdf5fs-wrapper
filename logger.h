aea048b0483eabc9cee41117a037ecc1955156b8
Henning Glawe <glaweh@debian.org>
2017-05-11 21:58:04 +0200

    forward log_level to log_msg_function

--- logger.h
+++ logger.h
@@ -30,40 +30,40 @@
 #define LOG_LEVEL_DBG2     (6)
 #define LOG_LEVEL_DBG3     (7)
 
-void log_msg_function(const char *function_name, const char *fstring, ...) __attribute__ ((format (printf, 2, 3)));
+void log_msg_function(const int log_level, const char *function_name, const char *fstring, ...) __attribute__ ((format (printf, 3, 4)));
 
 #if (LOG_LEVEL >= LOG_LEVEL_FATAL)
-#define LOG_FATAL(...) log_msg_function(__func__,__VA_ARGS__)
+#define LOG_FATAL(...) log_msg_function(LOG_LEVEL_FATAL, __func__, __VA_ARGS__)
 #else
 #define LOG_FATAL(...) do {} while(0)
 #endif
 #if (LOG_LEVEL >= LOG_LEVEL_ERR)
-#define LOG_ERR(...) log_msg_function(__func__,__VA_ARGS__)
+#define LOG_ERR(...) log_msg_function(LOG_LEVEL_ERR, __func__, __VA_ARGS__)
 #else
 #define LOG_ERR(...) do {} while(0)
 #endif
 #if (LOG_LEVEL >= LOG_LEVEL_WARN)
-#define LOG_WARN(...) log_msg_function(__func__,__VA_ARGS__)
+#define LOG_WARN(...) log_msg_function(LOG_LEVEL_WARN, __func__, __VA_ARGS__)
 #else
 #define LOG_WARN(...) do {} while(0)
 #endif
 #if (LOG_LEVEL >= LOG_LEVEL_INFO)
-#define LOG_INFO(...) log_msg_function(__func__,__VA_ARGS__)
+#define LOG_INFO(...) log_msg_function(LOG_LEVEL_INFO, __func__, __VA_ARGS__)
 #else
 #define LOG_INFO(...) do {} while(0)
 #endif
 #if (LOG_LEVEL >= LOG_LEVEL_DBG)
-#define LOG_DBG(...) log_msg_function(__func__,__VA_ARGS__)
+#define LOG_DBG(...) log_msg_function(LOG_LEVEL_DBG, __func__, __VA_ARGS__)
 #else
 #define LOG_DBG(...) do {} while(0)
 #endif
 #if (LOG_LEVEL >= LOG_LEVEL_DBG2)
-#define LOG_DBG2(...) log_msg_function(__func__,__VA_ARGS__)
+#define LOG_DBG2(...) log_msg_function(LOG_LEVEL_DBG2, __func__, __VA_ARGS__)
 #else
 #define LOG_DBG2(...) do {} while(0)
 #endif
 #if (LOG_LEVEL >= LOG_LEVEL_DBG3)
-#define LOG_DBG3(...) log_msg_function(__func__,__VA_ARGS__)
+#define LOG_DBG3(...) log_msg_function(LOG_LEVEL_DBG3, __func__, __VA_ARGS__)
 #else
 #define LOG_DBG3(...) do {} while(0)
 #endif
