8d0b4b8d1b33a3c8d954ab2f50c429e69f99a76f
Henning Glawe <glaweh@debian.org>
2017-05-12 13:15:04 +0200

    loglevel DBG for fd-still-open msg; explicitly close fd in this case

--- wrapper_func.c
+++ wrapper_func.c
@@ -45,7 +45,10 @@ void __attribute__ ((constructor(300))) wrapper_func_init(void) {
 void __attribute__ ((destructor(300)))  wrapper_func_fini(void) {
     khiter_t k;
     for (k = kh_begin(wrapper_fds); k != kh_end(wrapper_fds); ++k) {
-        if (kh_exist(wrapper_fds, k)) LOG_WARN("fd still open: %d\n",kh_key(wrapper_fds,k));
+        if (kh_exist(wrapper_fds, k)) {
+            LOG_DBG("fd still open: %d\n",kh_key(wrapper_fds,k));
+            h5fd_close(kh_value(wrapper_fds,k));
+        }
     }
     kh_destroy(WFILE,wrapper_files);
     kh_destroy(WFD,wrapper_fds);
