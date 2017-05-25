acffe9f7ef778091e67ea81e25f492556c6902dc
Henning Glawe <glaweh@debian.org>
2017-05-25 21:25:11 +0200

    remove useless include

--- path_util.c
+++ path_util.c
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with hdf5fs-wrapper.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "real_func_auto.h"
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
