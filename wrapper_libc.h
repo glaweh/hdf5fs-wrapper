261fd7416ea9b2e428c0c1732eb97c714d90cc0c
Henning Glawe <glaweh@debian.org>
2017-06-03 22:23:16 +0200

    include mandatory libc headers in wrapper_libc.h

--- wrapper_libc.h
+++ wrapper_libc.h
@@ -1,6 +1,17 @@
 #ifndef WRAPPER_LIBC_H
 #define WRAPPER_LIBC_H
 
+#define _GNU_SOURCE
+#define __USE_LARGEFILE64
+#include <dirent.h>
+#include <libio.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <wchar.h>
+#include <fcntl.h>
+
 #define PATHNAME char*
 #define FD int
 
