e6534ca1c619eb0f9d815b39c5b72cfd149e9b8c
Henning Glawe <glaweh@debian.org>
2017-06-04 09:36:18 +0200

    add LGPL boilerplate and comment about purpose in wrapper_libc.h

--- wrapper_libc.h
+++ wrapper_libc.h
@@ -1,3 +1,28 @@
+/*
+ * This header defines/includes all symbols necessary to wrap libc symbols.
+ * Besides the normal symbols, we also define the macros which are
+ * referenced in wrapper-gen/io-calls.c
+ */
+
+/*
+ * Copyright (c) 2017 Henning Glawe <glaweh@debian.org>
+ *
+ * This file is part of hdf5fs-wrapper.
+ *
+ * hdf5fs-wrapper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * hdf5fs-wrapper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with hdf5fs-wrapper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef WRAPPER_LIBC_H
 #define WRAPPER_LIBC_H
 
